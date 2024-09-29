4aff8cd117a1c093db6bbf858039538033a65834
Nurmukhamed Abdilazizov <abdilaza12@gmail.com>
2024-09-29 10:58:38 +0600

    add method that fills gameBoard with water

--- Battleship.java
+++ Battleship.java
@@ -1,9 +1,27 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Battleship {
 
     public static void main(String[] args) {
         String name = requestPlayerName();
+        char water = '-';
+        char miss = 'O';
+        char hit = 'X';
+        char ship = 'S';
+        int gameBoardLength = 7;
+        char[][] gameBoard = createGameBoard(gameBoardLength, water);
+
+    }
+
+    private static char[][] createGameBoard(int gameBoardLength, char water) {
+        char[][] gameBoard = new char[gameBoardLength][gameBoardLength];
+
+        for (char[] i: gameBoard) {
+            Arrays.fill(i, water);
+        }
+
+        return gameBoard;
     }
 
     private static String requestPlayerName() {
